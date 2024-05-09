2633087c0f4e4783f24d22fcb6fd894a3765025e
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-05-09 18:01:43 +0000

    type conversion

---
+++ JS_basics/conversionsOperations.js
@@ -0,0 +1,24 @@
+let score = "18Mar"
+
+//console.log(score);
+//console.log(typeof score);
+
+let scoreNumber = Number(score)
+// "18Mar" => NaN
+// "18" => 18
+// true => 1; false => 0
+// undefined => undefined
+//console.log(typeof scoreNumber);
+//console.log(scoreNumber);
+let isLoggedIn = "Goku"
+let isLoggedInBool = Boolean(isLoggedIn)
+//console.log(isLoggedInBool);
+// "Goku" => true
+// "" => false
+// "1" => true, "0" => false
+
+let custId = 29947
+
+let custIdString = String(custId)
+console.log(typeof custIdString);
+console.log(custIdString);
